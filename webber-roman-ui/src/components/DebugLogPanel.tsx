cee07be49cab1b2fc599db47c36706ac65182c32
Caelan Sayler <git@caesay.com>
2026-06-28 16:37:32 +0100

    Remove roman UI

--- webber-roman-ui/src/components/DebugLogPanel.tsx
+++
@@ -1,8 +0,0 @@
-import { useDebugBlock } from "../blocks/DebugBlock";
-
-export function DebugLogPanel(props: React.HTMLAttributes<HTMLDivElement>): React.ReactNode {
-    const { logs } = useDebugBlock();
-    return <div {...props}>
-        {logs.map(s => <p>{s}</p>)}
-    </div>
-}
--- webber-roman-ui/src/config.ts
+++
@@ -1,4 +0,0 @@
-export const Config = {
-    ServerUrl: window.location.protocol + "//" + window.location.host,
-    //ServerUrl: "http://localhost:1234",
-};
